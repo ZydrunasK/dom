ce2e616dcc1295dfad284e40edc1210085d3a9e5
Zydrunask <kniukstaz@gmail.com>
2024-08-27 19:47:47 +0300

    scoreboard finished

---
+++ README.md
@@ -0,0 +1,24 @@
+ # playing with DOM
+
+Website project to learn how to use DOM
+
+
+You can see it <a href="https://zydrunask.github.io/workshop-dom/">here</a>
+
+
+## About
+
+This project contains several pages with diferent contents made with the help of a fella we call DOM.
+
+
+## Project feature/goal
+- githup pages
+- javascript
+- CSS file
+- **NO** responsive design
+
+<br>
+
+## Author
+
+Žydrūnas: [Github](https://github.com/ZydrunasK)
--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,15 +1,17 @@
 import { header } from './header.js';
 header('scoreboard');
 
-const homeDOM = document.getElementsByClassName('.board:first-child');
 const [homeOne, homeTwo, homeThree] = document.querySelectorAll('.home .btn button');
 const [homeScore] = document.querySelectorAll('.home .score');
 
-const guestDOM = document.getElementsByClassName('guest');
 const [guestOne, guestTwo, guestThree] = document.querySelectorAll('.guest .btn button');
 const [guestScore] = document.querySelectorAll('.guest .score');
 
-console.log(document.querySelectorAll('.guest .score'));
+const periodBtnDOM = document.querySelector('.periodBtn');
+const pHomeDOM = document.querySelectorAll('.perHome p');
+const pGuestDOM = document.querySelectorAll('.perGuest p');
+const thePeriodDOM = document.querySelector('.thePeriod');
+
 
 
 let homeCount = 0;
@@ -42,3 +44,42 @@ homeThree.addEventListener('click', upThreeH)
 guestOne.addEventListener('click', upOneG)
 guestTwo.addEventListener('click', upTwoG)
 guestThree.addEventListener('click', upThreeG)
+
+console.log(thePeriodDOM.textContent === `P${0 + 1}`);
+
+function endPeriod() {
+    if (thePeriodDOM.textContent === 'FIN') {
+        for (let i = 0; i < 4; i++) {
+            thePeriodDOM.textContent = 'P1';
+            pHomeDOM[i].textContent = 0;
+            pGuestDOM[i].textContent = 0;
+            homeCount = 0;
+            guestCount = 0;
+            homeScore.textContent = 0;
+            guestScore.textContent = 0;
+            periodBtnDOM.textContent = 'End Period';
+            if (i === 3) {
+                return '';
+            }
+        }  
+    }
+    for (let i = 0; i < 4; i++) {
+            if (thePeriodDOM.textContent === `P${i + 1}`) {
+            pHomeDOM[i].textContent = 0 + homeCount;
+            pGuestDOM[i].textContent = 0 + guestCount;
+            break;
+        }  
+    }
+    for (let i = 0; i < 4; i++) {
+        if (i === 3) {
+         thePeriodDOM.textContent = 'FIN';
+         periodBtnDOM.textContent = 'RESTART' 
+        }
+        if (thePeriodDOM.textContent === `P${i + 1}`) {
+        thePeriodDOM.textContent = `P${++i + 1}`; 
+        break;
+       }
+    }
+} 
+
+periodBtnDOM.addEventListener('click', endPeriod)
